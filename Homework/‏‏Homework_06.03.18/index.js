23dd5aad4ad4718bd39059c3fafe8c258b11d312
amirtaib <35423903+amirtaib@users.noreply.github.com>
2018-03-27 00:13:34 +0300

    Add files via upload

---
+++ Homework/‏‏Homework_06.03.18/index.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+let counterFamelyPizza = 0;
+let counterPersonalPizza = 0;
+let counterSalat = 0;
+
+
+
+function FamelyPizza() {
+    counterFamelyPizza++;
+}
+
+function PersonalPizza() {
+    counterPersonalPizza++;
+}
+
+function Salat() {
+    counterSalat++;
+}
+
+function FinishAnOrder() {
+
+}
