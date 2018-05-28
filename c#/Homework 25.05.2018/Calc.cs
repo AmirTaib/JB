8df6ce5a31ee936bf55eec4b6c450c8fc16d26d8
amirtaib <35423903+amirtaib@users.noreply.github.com>
2018-05-28 20:01:55 +0300

    Add files via upload

---
+++ c#/Homework 25.05.2018/Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_25._05._2018
+{
+    static class Calc
+    {
+
+        private static DateTime startDate;
+
+
+        public static int GetSub(int num1, int num2)
+        {
+            return num1+num2;
+        }
+        public static int GetSub(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+        public static double GetSub(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        static Calc()
+        {
+            startDate = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine($" This is the first use of the calc \n The time now is {startDate}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ c#/Homework 25.05.2018/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_25._05._2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            
+            int int1 = rnd.Next(0, 101);
+            int int2 = rnd.Next(0, 101);
+            int int3 = rnd.Next(0, 101);
+
+            double d1 = rnd.Next(0, 101);
+            double d2 = rnd.Next(0, 101);
+
+            Console.WriteLine(" Call to function with two int number:");
+            Console.WriteLine($" int1: {int1},   int2: {int2}, \n sum of them is:{Calc.GetSub(int1, int2)}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(" Call to function with three int number:");
+            Console.WriteLine($" int1: {int1},   int2: {int2},   int3: {int3}, \n sum of them is:{Calc.GetSub(int1, int2, int3)}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(" Call to function with two int number:");
+            Console.WriteLine($" d1: {d1},   d2: {d2}, \n sum of them is:{Calc.GetSub(d1, d2)}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+        }
+    }
+}
