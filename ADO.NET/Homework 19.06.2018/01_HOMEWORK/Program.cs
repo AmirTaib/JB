0ef2320cf49ab1f24d36873ba04f2db51227f5df
amirtaib <35423903+amirtaib@users.noreply.github.com>
2018-06-21 23:26:57 +0300

    Add files via upload

---
+++ ADO.NET/Homework 19.06.2018/01_HOMEWORK/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_HOMEWORK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<dynamic> customerList = new List<dynamic>();
+
+            string connectionString = @"Data Source=אמיר\sqlexpress;Initial Catalog=northwind;Integrated Security=True";
+
+            SqlConnection sql = null;
+
+            try
+            {
+                using (sql = new SqlConnection(connectionString))
+                {
+                    sql.Open();
+                    
+
+                    SqlCommand query = new SqlCommand("SELECT [CustomerID],[CompanyName],[Address],[City],[Region],[Country] FROM Customers", sql);
+
+
+                    SqlDataReader sqlDataReader = query.ExecuteReader();
+
+
+
+                    while (sqlDataReader.Read())
+                    {
+                        customerList.Add(new
+                        {
+                            ID = sqlDataReader[0],
+                            CompanyName = sqlDataReader[1],
+                            Address = sqlDataReader[2],
+                            City = sqlDataReader[3],
+                            Region = sqlDataReader[4],
+                            Country = sqlDataReader[5]
+                        });
+
+
+                    }
+
+                    foreach (var item in customerList)
+                    {
+                        Console.WriteLine($"ID-{item.ID}\nCompany name:{item.CompanyName}\nAddress:{item.Address}\nCity:{item.City}\nRegion:{item.Region}\nCountry:{item.Country}");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                    }
+
+                    Console.Read();
+
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
+
---
+++ ADO.NET/Homework 19.06.2018/basic_query/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString = "Data Source=אמיר\\sqlexpress;Initial Catalog=northwind;Integrated Security=True";
+
+            SqlConnection sql = null;
+
+            try
+            {
+                using (sql = new SqlConnection(connectionString))
+                {
+                    sql.Open();
+
+                    Console.WriteLine("Please enter min price:");
+                    int.TryParse(Console.ReadLine(), out int minPrice);
+
+
+                    Console.WriteLine("Please enter max price:");
+                    int.TryParse(Console.ReadLine(), out int maxPrice);
+
+
+                    SqlCommand query = new SqlCommand($"SELECT [ProductName], [UnitPrice] FROM [dbo].[Products] WHERE [UnitPrice] BETWEEN {minPrice} AND {maxPrice}", sql);
+
+
+                    SqlDataReader sqlDataReader = query.ExecuteReader();
+
+
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine($"{sqlDataReader[0]} costs {(decimal)sqlDataReader[1]}");
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
