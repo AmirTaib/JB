0771607ac0e17e8d2baba614be1a5aca779bd2e5
amirtaib <35423903+amirtaib@users.noreply.github.com>
2018-06-28 21:41:16 +0300

    Add files via upload

---
+++ ADO.NET/Homework 26.06.2018/northwind_filter_ef/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace northwind_filter_ef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (northwindEntities ef = new northwindEntities())
+            {
+
+                IEnumerable<Product> productDiscounte = ef.Products.Select(p => p);
+                foreach (var item in productDiscounte)
+                {
+                    Console.WriteLine($"ProductName: {item.ProductName}, UnitPrice: {item.UnitPrice}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                productDiscounte = ef.Products.Where(p => p.Category.CategoryName.Length == p.ProductName.Length )
+                    .Select(p=> p);
+
+
+                foreach (var item in productDiscounte)
+                {
+                    item.UnitPrice = item.UnitPrice - (item.UnitPrice / 10);
+                }
+
+
+                ef.SaveChanges();
+
+                foreach (var item in productDiscounte)
+                {
+                    Console.WriteLine($"ProductName: {item.ProductName}, Price after 10% discount : {item.UnitPrice}");
+                }
+
+            }
+
+        }
+}
+}
